f9608d5e60119ae379fa83c17b53e1ace40e1297
knightprince-png <mwaurafelix754@gmail.com>
2025-02-03 13:42:58 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to display a value
+print("Felix")
+print("Emobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,20 @@
+#variable is a container used to store a value
+
+number = 10
+firstname="felix"
+lastname="mwaura"
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+
+language="python"
+print(language,"is a web programming language")
+
+a=10
+b=15
+c=25
+print(a+b)
+
+#assignment
+print("The sum of",a,"and",b,"is",c)
