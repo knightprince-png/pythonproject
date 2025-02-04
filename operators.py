28b196cf4cb1fbe5a622601607cbcb221215294e
knightprince-png <mwaurafelix754@gmail.com>
2025-02-04 13:14:30 +0300

    second commit

---
+++ operators.py
@@ -0,0 +1,28 @@
+# Arithmetic operators
+x=50
+y=32
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y) # modulus - returns remainder after division
+
+#comparison operators
+print(x<y)
+print(x>y)
+print(x>=y)
+print(x<=y)
+print(x==y)
+print(x!=y)
+
+#assignment operators
+a=10
+
+a +=2
+
+print(a)
+
+#logical operators -and, or,not
+print(34<50 and 56>200)
+print(34 < 50 or 56 > 200)
+print(not(34<50 or 56>200))
