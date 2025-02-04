67a3eb58e94e7b904a568c64b0ef8d7987f0cfdc
knightprince-png <mwaurafelix754@gmail.com>
2025-02-04 12:55:23 +0300

    initial commit

---
+++ datatype.py
@@ -0,0 +1,27 @@
+number=45 # int
+num=66.78 # float
+text="Hello" # str
+isMammal = True # Boolean
+
+print(number)
+
+#Data structures
+cars = ["Toyota","Mercedes","Ford"] # list
+fruits = ("Banana","Apple","Passion") # Tuple
+countries = { "England", "Italy", "Germany"} # Set
+details = {
+    "firstname" : "Brian",
+    "course" : "MIT",
+    "Age" : 19,
+    "Maritalstatus" : "single"
+}
+
+print(cars)
+print(fruits)
+print(countries)
+print(details)
+print(details["course"])
+
+#Typecasting
+print(float(number))
+print(int(num))
---
+++ string.py
@@ -0,0 +1,22 @@
+text = "Hello World"
+language = "PYTHON"
+greetings="Hello"
+name="Felix"
+print(text)
+print(language)
+
+#Accessing an element in a string
+print(text[1])
+
+#size of a string
+print(len(text))
+
+#modifying a string
+print(text.upper())
+print(language.lower())
+
+#string concatenation - joining strings
+print(greetings+name)
+print(greetings + " " + name)
+
+
